9e06088ef75ef78dd8ad8dfd092d8cbb230b9e2d
NitikGit <nimanitik@gmail.com>
2025-12-29 20:25:11 +0545

    Initial commit and SignIn UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import SignUp from './SignUp'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
