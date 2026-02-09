1ce40f97c3fc7e6498e1dc3c94f733a8d8759641
NitikGit <nimanitik@gmail.com>
2026-02-09 21:21:36 +0545

    Update SignIn function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import SignUp from './SignUp'
+import SignIn from './SignIn'
 import './App.css'
 
 function App() {
   return (
     <div>
-      <SignUp/>
+      <SignIn/>
     </div>
   );
 }
